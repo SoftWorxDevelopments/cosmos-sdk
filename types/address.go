11d3132ad45f42711d482a807ac2f8bebd4f578e
SoftWorx <58102307+SoftWorxDevelopments@users.noreply.github.com>
2020-08-19 15:53:24 +0200

    Update address.go

--- types/address.go
+++ types/address.go
@@ -35,11 +35,11 @@ const (
 	Bech32MainPrefix = "mypos"
 
 	// Atom in https://github.com/satoshilabs/slips/blob/master/slip-0044.md
-	CoinType = 688
+	CoinType = 118
 
 	// BIP44Prefix is the parts of the BIP44 HD path that are fixed by
 	// what we used during the fundraiser.
-	FullFundraiserPath = "44'/688'/0'/0/0"
+	FullFundraiserPath = "44'/118'/0'/0/0"
 
 	// PrefixAccount is the prefix for account keys
 	PrefixAccount = "acc"
